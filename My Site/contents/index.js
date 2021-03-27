cbb9974e23cd030ec6e53750964e9d51dbfd4d01
shalnsingh@outlook.com <shalnsingh@outlook.com>
2021-03-27 11:48:23 +0530

    adding files

---
+++ My Site/contents/index.js
@@ -0,0 +1,9 @@
+var menuBtn=document.getElementById("menuBtn")
+var menu=document.getElementById("menu")
+
+
+menuBtn.onclick=function(){
+var element = document.body;
+   element.classList.toggle("dark-mode");
+    
+}
